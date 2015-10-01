9c7d7936bfde2e112a7c6ab102782f9425b1b832
Cody Antcliffe <codyantcliffe@gmail.com>
2015-10-01 14:57:53 -0400

    Added tabs to lines

--- src/Room.java
+++ src/Room.java
@@ -1,36 +1,36 @@
 /* Holds info for the room */
 public class Room {
 	
-int number;
-Facility fac;// Which facility it belongs to
-Resident occupant; //Which Resident is currently in the room
-
-public Room(int rmNum, Facility F){
+	int number;
+	Facility fac;// Which facility it belongs to
+	Resident occupant; //Which Resident is currently in the room
 	
-	this.number = rmNum;
-	this.fac = F;
-	this.occupant = null; // Set to null since it hasn't been assigned 
-}
-
-//Check if room is currently occupied
-public Boolean isOccupied(){
-	
-	if(this.occupant == null){
-		return false;
+	public Room(int rmNum, Facility F){
+		
+		this.number = rmNum;
+		this.fac = F;
+		this.occupant = null; // Set to null since it hasn't been assigned 
 	}
-	else
-		return true;
-}
-
-//Assign a resident to the room
-public void assign(Resident R){
 	
-	if(this.isOccupied()){
-		System.out.println("Room is already in use.");
+	//Check if room is currently occupied
+	public Boolean isOccupied(){
+		
+		if(this.occupant == null){
+			return false;
+		}
+		else
+			return true;
 	}
-	else
-		this.occupant = R;
-}
+	
+	//Assign a resident to the room
+	public void assign(Resident R){
+		
+		if(this.isOccupied()){
+			System.out.println("Room is already in use.");
+		}
+		else
+			this.occupant = R;
+	}
 
 
 }
