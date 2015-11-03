4f92e8418818ddd4e42233bd114ee7763c75ac88
Cody Antcliffe <codyantcliffe@gmail.com>
2015-11-03 13:27:46 -0500

    Added to-do list for version 1.

--- src/Controls/mainClass.java
+++ src/Controls/mainClass.java
@@ -18,6 +18,19 @@ public class mainClass {
 		
 		Resident currentResident = Register.acceptApplicant(currentApplicant);
 		
+		/*
+		 * Login and register should be within driver class
+		 * Manager driver and student driver, extends driver
+		 * create student class which combines resident and applicant.  Have access to student driver.  
+		 * ManagerDriver and StudentDrive have different access rights to the database. 
+		 * Room class, need to add to db.  
+		 * Web-page to print out rooms with drop down menus.
+		 * Methods to get array of rooms names.  ViewRoomInfo returns object of type room.  
+		 * Table for applications. Student can create an application via ApplyForResidence.Manager can access these via ManagerDriver, where it returns array fo these objects.
+		 * All driver call should be encapsulated by a method call in Student or Manager or User.
+		 * Login should first check if credentials exist/true.  Return accountType if successful, or null otherwise.
+		 * As many driver access methods as we can think of.
+		 */
 	
 	}
 }
